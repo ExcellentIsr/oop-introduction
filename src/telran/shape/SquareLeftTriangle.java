7fa99a563c75f117fdb23b5c9d6a4c77f694d136
Danil Katz <i_like_it74@mail.ru>
2022-12-10 01:37:06 +0200

    BaseCipher

--- src/telran/shape/SquareLeftTriangle.java
+++
@@ -1,9 +0,0 @@
-package telran.shape;
-
-public class SquareLeftTriangle extends SquareTriangle {
-
-	public SquareLeftTriangle(int width) {
-		super(width, true);
-	}
-
-}
--- src/telran/shape/SquareRightTriangle.java
+++
@@ -1,9 +0,0 @@
-package telran.shape;
-
-public class SquareRightTriangle extends SquareTriangle {
-
-	public SquareRightTriangle(int width) {
-		super(width, false);
-	}
-
-}
